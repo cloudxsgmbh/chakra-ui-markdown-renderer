3f31b676aeff1a4c4c50461d94fa78fcfcf0b96f
Andy Brunner <andy@cloudxs.ch>
2025-11-26 08:54:57 +0100

    👷 Add release-it

---
+++ .release-it.ts
@@ -0,0 +1,27 @@
+/* © 2025 cloudxs GmbH. All rights reserved. / .release-it.ts */
+
+import type { Config } from 'release-it';
+
+export default {
+  git: {
+    commit: true,
+    tag: true,
+    push: true,
+    requireBranch: 'main',
+  },
+  github: {
+    release: false,
+  },
+  npm: {
+    publish: true,
+  },
+  hooks: {
+    'before:init': ['git pull', 'bun run lint', 'bun test'],
+  },
+  plugins: {
+    '@release-it/conventional-changelog': {
+      preset: 'eslint',
+      infile: 'CHANGELOG.md',
+    },
+  },
+} satisfies Config;
